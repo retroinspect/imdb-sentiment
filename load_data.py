fix bug: add padding in front of the input tensors to prevent vanishing gradient

load_data.py
@@ -4,11 +4,9 @@ methods & classes on dataset, batching
 """
 
 import pandas as pd
-import csv
 import torch
 from torch.utils.data import Dataset, random_split
-
-from utils import pad, MyTokenizer
+from utils import pad, preprocess_text, tokenize_text
 
 def split_data(dataset, train=0.4, val=0.1, test=0.5):
   total = len(dataset)
@@ -57,13 +55,14 @@ class MyCollator(object):
 
   def __init__(self, vocabulary):
     self.vocabulary = vocabulary
-    self.tokenizer = MyTokenizer()
     
   def __call__(self, batch): # batch example: {label: 'positive', text: 'good movie'}
     labels, texts = [], []
     for sample in batch:
-      label, txt = sample['label'], sample['text']
-      texts.append(self.tokenizer(txt))
+      label, text = sample['label'], sample['text']
+      preprocessed = preprocess_text(text)
+      tokenized = tokenize_text(preprocessed)
+      texts.append(tokenized)
       labels.append(label)
     
     text_lengths = torch.LongTensor([len(text) for text in texts])

main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from numpy import Inf
 
 from utils import build_vocab
 from load_data import IMDBDataset, split_data, MyCollator
@@ -43,6 +44,7 @@ print(f'vocab size: {vocab_size}')
 collate_fn = MyCollator(vocabulary)
 train_loader = DataLoader(train_set, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
 valid_loader = DataLoader(valid_set, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
 
 """
 model configuration
@@ -58,15 +60,23 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 """
 train & validate
 """
+valid_loss_min = Inf
 for iter in range(n_iters):
   train_loss, train_accuracy = train(model, optimizer, train_loader)
-  valid_loss, valid_accuracy, valid_known_rate = validate(model, valid_loader)
+  valid_loss, valid_accuracy = validate(model, valid_loader)
 
   print(f"[{iter+1}/{n_iters}] Train Loss : {train_loss:.4f} Train Acc : {train_accuracy:.2f} \
-  Valid Loss : {valid_loss:.4f} Valid Acc : {valid_accuracy:.2f} Valid Word in vocabulary : {valid_known_rate:.2f}")
-
-torch.save(model, 'imdb-rnn-classification.pt')
+  Valid Loss : {valid_loss:.4f} Valid Acc : {valid_accuracy:.2f}")
+  if (valid_loss < valid_loss_min):
+    torch.save(model.state_dict(), 'best-model-state.pt')
+    valid_loss_min = valid_loss
 
 """
-TODO test
+test
 """
+model = GRU(embedding_size, n_hidden, n_classes, vocab_size)
+if useGPU:
+  model.to(device)
+model.load_state_dict(torch.load('./best-model-state.pt'))
+test_loss, test_accuracy = validate(model, test_loader, useGPU, desc="testing...")
+print(f"Test Loss : {test_loss:.4f} Train Acc : {test_accuracy:.2f}")

model.py
@@ -61,7 +61,7 @@ class GRU(nn.Module):
           self.embedding_layer = pretrained_embedding
         else:
           # init embeddings with random tensor
-          self.embedding_layer = nn.Embedding(num_embeddings=vocab_size, embedding_dim=input_size, padding_idx=1)
+          self.embedding_layer = nn.Embedding(num_embeddings=vocab_size, embedding_dim=input_size, padding_idx=0)
 
         self.hidden_size = hidden_size
         self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size)

utils.py
@@ -8,37 +8,31 @@ from tqdm import tqdm
 import numpy as np
 import re
 
-unk_token = '<unk>'
 pad_token = '<pad>'
 
-class MyTokenizer(object):
-  def _preprocess_string(self, s):
-    s = re.sub('[=+,#/\?:^$.@*\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', s) # remove special characters
-    s = re.sub(r"\s+", ' ', s) # replace multiple spaces into single space
-    s = re.sub(r"\d", '', s) # remove digit
-    return s
+def preprocess_text(word):
+  word = re.sub(r"[^\w\s]", '', word)
+  word = re.sub(r"\s+", ' ', word) # replace multiple spaces into single space
+  word = re.sub(r"\d", '', word)  # remove digit
+  return word
 
-  def __init__(self, final_tokenizer="basic_english"):
-    self.final_tokenizer = get_tokenizer(final_tokenizer)
-
-  def __call__(self, string):
-    preprocessed = self._preprocess_string(string)
-    return self.final_tokenizer(preprocessed)
-
+def tokenize_text(text):
+  tokenized = get_tokenizer("basic_english")(text)
+  return tokenized
 
 def build_vocab(train, min_freq=5):
   words = []
-  tokenizer = MyTokenizer()
 
   for _, sample in enumerate(tqdm(train, desc=f"tokenizing train set")):
     text = sample['text']
-    words += tokenizer(text)
+    preprocessed = preprocess_text(text)
+    words += tokenize_text(preprocessed)
 
   print("counting words frequency")
   words = np.array(words, dtype=np.unicode_)
   unique, counts = np.unique(words, return_counts=True)
-  vocabulary = dict({unk_token: 0, pad_token: 1})
-  id = 2
+  vocabulary = dict({pad_token: 0})
+  id = 1
   print(f"sieving words appeared at least {min_freq} times")
   for freq, word in zip(counts, unique): 
     if (freq < min_freq):
@@ -49,7 +43,9 @@ def build_vocab(train, min_freq=5):
   return vocabulary
 
 def pad(text, max_len):
-  text += [pad_token] * (max_len - len(text))
+  pads = [pad_token] * (max_len - len(text))
+  text = pads + text # 이렇게 바꾸면 갑자기 잘 됨
+  # text = text + pads # 전혀 학습 안함
   return text
 
 def thresholding(prediction):

validate.py
@@ -4,12 +4,11 @@ from tqdm import tqdm
 
 from utils import thresholding
 
-def validate(model, valid_loader, useGPU=True):
+def validate(model, valid_loader, useGPU=True, desc="validating..."):
   model.eval()
   total_loss, correct_cnt = 0.0, 0.0
-  n_known, n_word, n_padding = 0.0, 0.0, 0.0
 
-  for batch_idx, sample_batched in enumerate(tqdm(valid_loader, desc="validating...")):
+  for batch_idx, sample_batched in enumerate(tqdm(valid_loader, desc=desc)):
     with torch.no_grad():
       label_tensor, text_tensor, update_bounds = sample_batched
 
@@ -17,10 +16,6 @@ def validate(model, valid_loader, useGPU=True):
         label_tensor = label_tensor.cuda()
         text_tensor = text_tensor.cuda()
         update_bounds = update_bounds.cuda()
-
-    n_word += torch.numel(text_tensor)
-    n_known += torch.count_nonzero(text_tensor).item()
-    n_padding += (text_tensor == 1).sum().item()
 
     pred = model(text_tensor, update_bounds)
     loss = F.cross_entropy(pred, label_tensor)
@@ -32,5 +27,4 @@ def validate(model, valid_loader, useGPU=True):
   total_cnt = len(valid_loader.dataset)
   accuracy = correct_cnt * 1.0 / total_cnt  # calculate accuracy  (#accumulated-correct-prediction/#accumulated-data)
   loss = total_loss / total_cnt # calculate and save loss (#accumulated-loss/#accumulated-data)
-  known_rate = (n_known - n_padding) * 1.0 / (n_word - n_padding)
-  return loss, accuracy, known_rate
+  return loss, accuracy